a537ccf47368885a4e68ffa474e5ea7fff83117a
Gukss <dongkuk97@naver.com>
2023-03-24 16:30:36 +0900

    FEAT: 통계 read 완료

---
+++ BE/candy/src/main/java/com/project/candy/beer_history/repository/BeerHistoryRepository.java
@@ -0,0 +1,16 @@
+package com.project.candy.beer_history.repository;
+
+import com.project.candy.beer_history.entity.BeerHistory;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+/**
+ * packageName    : com.project.candy.beer_history.repository fileName       : BeerHistoryRepository
+ * date           : 2023-03-24 description    :
+ */
+public interface BeerHistoryRepository extends JpaRepository<BeerHistory, Long> {
+  @Query("select bh from BeerHistory bh join fetch bh.beer")
+  List<BeerHistory> findAllByUser();
+}
